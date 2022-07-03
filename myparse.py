a3e97b1caa930b611549d21131d495bf15910f49
Antonio <bronick@bigmir.net>
2022-07-03 18:05:01 +0300

    some improvements

--- myparse.py
+++ myparse.py
@@ -3,33 +3,37 @@ from requests_html import HTMLSession
 import codecs
 import webbrowser
 
-session = HTMLSession()
-r = session.get('https://shazoo.ru/tags/419?page=1')
-
-news_post = r.html.find('.flex.flex-col.gap-2.py-6')
-news_date = r.html.find('.flex.flex-col.gap-2.py-6 > .flex.items-center.gap-2 > time')
-news_header = r.html.find('.flex.flex-col.gap-2.py-6 > h4 > a')
-news_img = r.html.find('.flex.flex-col.gap-2.py-6 > .flex-shrink-0 > a > img')
-news_desc = r.html.find('.flex.flex-col.gap-2.py-6 > .break-words')
 news = []
 
+page = 1
+while page <= 2:
+	session = HTMLSession()
+	r = session.get('https://shazoo.ru/tags/419?page=' + str(page))
 
-i = 0
-while i < len(news_post):
-	temp = {
-			"header" : news_header[i].text,
-			"date" : news_date[i].text,
-			"link" : news_header[i].attrs["href"],
-			"img" : news_img[i].attrs["src"],
-			"desc" : news_desc[i].text
-	}
+	news_post = r.html.find('.flex.flex-col.gap-2.py-6')
+	news_date = r.html.find('.flex.flex-col.gap-2.py-6 > .flex.items-center.gap-2 > time')
+	news_header = r.html.find('.flex.flex-col.gap-2.py-6 > h4 > a')
+	news_img = r.html.find('.flex.flex-col.gap-2.py-6 > .flex-shrink-0 > a > img')
+	news_desc = r.html.find('.flex.flex-col.gap-2.py-6 > .break-words')
 
-	# json_temp = json.dumps(temp, indent = 2)
-	# news.append(json_temp)
-	# news.append('"p' + str(i) + '"' + " : " + temp)
-	
-	news.append(temp)
-	i += 1
+	i = 0
+	while i < len(news_post):
+		temp = {
+				"header" : news_header[i].text,
+				"date" : news_date[i].text,
+				"link" : news_header[i].attrs["href"],
+				"img" : news_img[i].attrs["src"],
+				"desc" : news_desc[i].text
+		}
+
+		# json_temp = json.dumps(temp, indent = 2)
+		# news.append(json_temp)
+		# news.append('"p' + str(i) + '"' + " : " + temp)
+		
+		news.append(temp)
+		i+=1
+		
+	page+=1
 
 # news_out = '\n'.join(news)
 # news_out = json.dumps(news, indent = 2)
